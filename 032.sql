e5d92b5e972490071ee28b8d85d72e5dc26ec0c6
donxd <n0s3.xd@gmail.com>
2019-10-02 14:00:59 -0500

    initial commit of arcade-db/032

---
+++ 032.sql
@@ -0,0 +1,5 @@
+/*Please add ; after each select statement*/
+CREATE PROCEDURE orderOfSuccession()
+BEGIN
+    SELECT CASE gender WHEN 'F' THEN CONCAT('Queen ', name) WHEN 'M' THEN CONCAT('King ', name) END AS name FROM Successors ORDER BY birthday ASC;
+END
